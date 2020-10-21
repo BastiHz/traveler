c5e478d3491fb053c5b2f714a10c13905cb89242
bastihz <bastihz.dev@posteo.de>
2020-10-21 14:10:55 +0200

    calculate distance

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import argparse
 import random
+from typing import List
 
 import pygame
+import pygame.freetype
 
 
 WINDOW_WIDTH = 1200
@@ -17,12 +19,34 @@ for x in range(MIN_DISTANCE, WINDOW_WIDTH, MIN_DISTANCE):
         VALID_POSITIONS.append(pygame.Vector2(x, y))
 
 
+def new_points(n: int) -> List:
+    return random.sample(VALID_POSITIONS, n)
+
+
+def get_distance(points: List[pygame.Vector2],
+                 closed: bool) -> float:
+    n_lines = len(points)
+    if not closed:
+        n_lines -= 1
+    distance = 0.0
+    for i in range(n_lines):
+        a = points[i]
+        b = points[(i + 1) % len(points)]
+        distance += a.distance_to(b)
+    return distance
+
+
 def run(n: int, path_open: bool) -> None:
     pygame.init()
     pygame.display.set_caption("traveling salesperson")
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     clock = pygame.time.Clock()
-    points = random.sample(VALID_POSITIONS, n)
+
+    points = new_points(n)
+    closed = not path_open
+
+    font = pygame.freetype.SysFont("inconsolate, consolas, monospace", 16)
+    font.fgcolor = (255, 255, 255)
 
     running = True
     while running:
@@ -34,13 +58,16 @@ def run(n: int, path_open: bool) -> None:
                 if event.key == pygame.K_ESCAPE:
                     running = False
                 elif event.key == pygame.K_n:
-                    points = random.sample(VALID_POSITIONS, n)
+                    points = new_points(n)
 
         window.fill(BACKGROUND_COLRO)
         for p in points:
             pygame.draw.circle(window, POINT_COLOR, p, POINT_RADIUS)
+        pygame.draw.aalines(window, POINT_COLOR, closed, points)
 
-        pygame.draw.aalines(window, POINT_COLOR, not path_open, points)
+        distance = get_distance(points, closed)
+        font.render_to(window, (5, 5), f"{distance=:.2f}")
+
         pygame.display.flip()
 
 
